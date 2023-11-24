74eec96aa83975ec9e5aa1c1a7826fccd9b45801
fengu <fengu0719@gmail.com>
2023-11-24 16:45:22 +0800

    feat(footer):實作待辦事項頁尾元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TodoComponent } from './todo/todo.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, TodoComponent],
+  imports: [HeaderComponent, TodoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
