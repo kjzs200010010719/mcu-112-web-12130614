e73435f63657bf16525a1fad5f7d2bf65d105f98
fengu <fengu0719@gmail.com>
2023-11-26 13:50:05 +0800

    feat(todo):實作顯示已完成日期

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,13 +7,13 @@ import {
   booleanAttribute,
   numberAttribute,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { parseHostBindings } from '@angular/compiler';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [DatePipe],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
 })
@@ -27,12 +27,18 @@ export class TodoComponent {
   @Input({ transform: booleanAttribute })
   hasFinished!: boolean;
   @Output()
-  readonly hasFinishedChange = new EventEmitter();
+  readonly hasFinishedChange = new EventEmitter<boolean>();
+
+  @Input()
+  finishDate?: Date;
+  @Output()
+  readonly finishDateChange = new EventEmitter<Date | undefined>();
 
   @HostBinding('class')
   class = 'app-todo';
 
   onSetStatus(hasFinished: boolean): void {
     this.hasFinishedChange.emit(hasFinished);
+    this.finishDateChange.emit(hasFinished ? new Date() : undefined);
   }
 }
